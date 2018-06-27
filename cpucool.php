e77e82ee79e603821b6abd175cfe0b64775479df
theinizio <theinizio@gmail.com>
2018-06-27 20:53:50 +0300

    Update cpucool.php

--- cpucool.php
+++ cpucool.php
@@ -8,10 +8,10 @@
 	    
 	    
 	    if($temp>45000)
-			$output=exec('gate 2 o l',$buf);
+			$output=exec('gate 8 o l',$buf);
 		
 		if($temp<40000)
-			$output=exec('gate 2 o h',$buf);
+			$output=exec('gate 8 o h',$buf);
 		echo $temp/1000;
 		echo " \xc2\xb0\C\n";
 	sleep(5);
